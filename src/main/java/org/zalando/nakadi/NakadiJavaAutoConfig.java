73fd19095a1fcdc16df9a463ad7213d40fbb9723
Felix Roske <felix@zalando.de>
2025-07-02 22:53:13 +0200

    Start naively

--- src/main/java/org/zalando/nakadi/NakadiJavaAutoConfig.java
+++ src/main/java/org/zalando/nakadi/NakadiJavaAutoConfig.java
@@ -1,18 +1,28 @@
 package org.zalando.nakadi;
 
 import nakadi.NakadiClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(NakadiProperties.class)
 public class NakadiJavaAutoConfig {
 
+    private final NakadiProperties properties;
+
+    public NakadiJavaAutoConfig(NakadiProperties properties) {
+        this.properties = properties;
+    }
+
     @Bean
+    @ConditionalOnMissingBean
     public NakadiClient nakadiClient() {
         var builder = NakadiClient.newBuilder();
-        builder.baseURI("http://localhost:8080");
+
+        builder.baseURI(properties.getBaseURI());
 
         return builder.build();
     }
 }
-
